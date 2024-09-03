9b7ee543b0d10f8cc947e92f6929ed363a55e5d9
jigar99 <jigarvaishnav06@gmail.com>
2024-09-02 22:51:52 -0500

    Initial Commit

---
+++ TDD/TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDApp.swift
+//  TDD
+//
+//  Created by Jigar on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
